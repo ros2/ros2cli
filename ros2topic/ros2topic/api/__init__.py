f435d1f40d7617295e22477fd9977aeafb070f60
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2017-06-12 10:20:55 -0700

    add ros2topic echo, list, pub including previous tests for yaml/csv output

---
+++ ros2topic/ros2topic/api/__init__.py
@@ -0,0 +1,46 @@
+# Copyright 2017 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ros2cli.node.strategy import NodeStrategy
+
+HIDDEN_TOPIC_PREFIX = '_'
+
+
+def get_topic_names_and_types(*, node, include_hidden_topics=False):
+    topic_names_and_types = node.get_topic_names_and_types()
+    if not include_hidden_topics:
+        topic_names_and_types = [
+            (n, t) for (n, t) in topic_names_and_types
+            if not t.startswith(HIDDEN_TOPIC_PREFIX)]
+    return topic_names_and_types
+
+
+def get_topic_names(*, node, include_hidden_topics=False):
+    topic_names_and_types = get_topic_names_and_types(
+        node=node, include_hidden_topics=include_hidden_topics)
+    return [n for (n, t) in topic_names_and_types]
+
+
+class TopicNameCompleter(object):
+    """Callable returning a list of topic names."""
+
+    def __init__(self, *, include_hidden_topics_key=None):
+        self.include_hidden_topics_key = include_hidden_topics_key
+
+    def __call__(self, prefix, parsed_args, **kwargs):
+        with NodeStrategy(parsed_args) as node:
+            return get_topic_names(
+                node=node,
+                include_hidden_topics=getattr(
+                    parsed_args, self.include_hidden_topics_key))
---
+++ ros2topic/ros2topic/verb/list.py
@@ -0,0 +1,47 @@
+# Copyright 2017 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ros2cli.node.strategy import add_arguments
+from ros2cli.node.strategy import NodeStrategy
+from ros2topic.api import get_topic_names_and_types
+from ros2topic.verb import VerbExtension
+
+HIDDEN_TOPIC_PREFIX = '_'
+
+
+class ListVerb(VerbExtension):
+    """Output a list of available topics."""
+
+    def add_arguments(self, parser, cli_name):
+        add_arguments(parser)
+        parser.add_argument(
+            '--show-type', action='store_true',
+            help='Additionally show the topic type')
+        parser.add_argument(
+            '-c', '--count-topics', action='store_true',
+            help='Only display the number of topics discovered')
+
+    def main(self, *, args):
+        with NodeStrategy(args) as node:
+            topic_names_and_types = get_topic_names_and_types(
+                node=node, include_hidden_topics=args.include_hidden_topics)
+
+        if args.count_topics:
+            print(len(topic_names_and_types))
+        elif topic_names_and_types:
+            for (topic_name, topic_type) in topic_names_and_types:
+                msg = '{topic_name}'
+                if args.show_type:
+                    msg += ' [{topic_type}]'
+                print(msg.format_map(locals()))
