3caf8de3719c8c62d2c73de6446128a053ceb7eb
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2017-06-12 10:08:00 -0700

    add argcomplete-based completion

--- ros2cli/setup.py
+++ ros2cli/setup.py
@@ -5,6 +5,12 @@ setup(
     name='ros2cli',
     version='0.0.0',
     packages=find_packages(exclude=['test']),
+    extras_require={
+        'completion': ['argcomplete'],
+    },
+    data_files=[
+        ('share/ros2cli/environment', ['completion/ros2-argcomplete.bash']),
+    ],
     author='Dirk Thomas',
     author_email='dthomas@osrfoundation.org',
     maintainer='Dirk Thomas',
