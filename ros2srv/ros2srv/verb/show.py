39ce5ab00d9b46b1920fd873e171a34c677eea88
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2017-06-13 15:12:51 -0700

    add ros2srv list, package, packages, show

---
+++ ros2srv/ros2srv/verb/show.py
@@ -0,0 +1,41 @@
+# Copyright 2017 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ros2srv.api import get_service_path
+from ros2srv.api import service_type_completer
+from ros2srv.verb import VerbExtension
+
+
+class ShowVerb(VerbExtension):
+    """Output the service definition."""
+
+    def add_arguments(self, parser, cli_name):
+        arg = parser.add_argument(
+            'service_type',
+            help="Type of the ROS service (e.g. 'std_srvs/Trigger')")
+        arg.completer = service_type_completer
+
+    def main(self, *, args):
+        # TODO(dirk-thomas) this logic should come from a rosidl related
+        # package
+        try:
+            package_name, service_name = args.service_type.split('/', 2)
+        except ValueError:
+            raise RuntimeError('The passed service type is invalid')
+        try:
+            path = get_service_path(package_name, service_name)
+        except LookupError as e:
+            return str(e)
+        with open(path, 'r') as h:
+            print(h.read(), end='')
---
+++ ros2srv/setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='ros2srv',
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    install_requires=['ros2cli'],
+    author='Dirk Thomas',
+    author_email='dthomas@osrfoundation.org',
+    maintainer='Dirk Thomas',
+    maintainer_email='dthomas@osrfoundation.org',
+    url='https://github.com/ros2/ros2cli/tree/master/ros2srv',
+    download_url='https://github.com/ros2/ros2cli/releases',
+    keywords=[],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+    ],
+    description='The srv command for ROS 2 command line tools.',
+    long_description="""\
+The package provides the srv command for the ROS 2 command line tools.""",
+    license='Apache License, Version 2.0',
+    test_suite='test',
+    entry_points={
+        'ros2cli.command': [
+            'srv = ros2srv.command.srv:SrvCommand',
+        ],
+        'ros2cli.extension_point': [
+            'ros2srv.verb = ros2srv.verb:VerbExtension',
+        ],
+        'ros2srv.verb': [
+            'list = ros2srv.verb.list:ListVerb',
+            'package = ros2srv.verb.package:PackageVerb',
+            'packages = ros2srv.verb.packages:PackagesVerb',
+            'show = ros2srv.verb.show:ShowVerb',
+        ],
+    }
+)
