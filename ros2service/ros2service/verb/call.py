c67de686e2a44346926f3583741793413390222e
William Woodall <william@osrfoundation.org>
2017-06-15 18:06:06 -0700

    improve shutdown behavior of call, add loop option

--- ros2service/ros2service/verb/call.py
+++ ros2service/ros2service/verb/call.py
@@ -12,8 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
 import importlib
 import json
+import time
 
 import rclpy
 from ros2service.api import ServiceNameCompleter
@@ -26,7 +28,7 @@ class CallVerb(VerbExtension):
     def add_arguments(self, parser, cli_name):
         arg = parser.add_argument(
             'service_name',
-            help="Name of the ROS service to publish to (e.g. '/chatter')")
+            help="Name of the ROS service to call to (e.g. '/add_two_ints')")
         arg.completer = ServiceNameCompleter(
             include_hidden_services_key='include_hidden_services')
         parser.add_argument(
@@ -38,27 +40,38 @@ class CallVerb(VerbExtension):
                  '(e.g. {"a": 1, "b": 2}), ' +
                  'otherwise the service request will be published with default values')
 
+        def float_or_int(input_string):
+            try:
+                value = float(input_string)
+            except ValueError:
+                raise argparse.ArgumentTypeError("'%s' is not an integer or float" % input_string)
+            return value
+
+        parser.add_argument(
+            '-l', '--loop', metavar='N', default=None, type=float_or_int,
+            help='Repeat the call every N seconds')
+
     def main(self, *, args):
         return main(args)
 
 
 def main(args):
-    requester(args.service_type, args.service_name, args.values)
+    requester(args.service_type, args.service_name, args.values, args.loop)
 
 
-def requester(service_type, service_name, values):
+def requester(service_type, service_name, values, loop):
     # TODO(wjwwood) this logic should come from a rosidl related package
     try:
-        package_name, service_name = service_type.split('/', 2)
+        package_name, srv_name = service_type.split('/', 2)
     except ValueError:
         raise RuntimeError('The passed service type is invalid')
     module = importlib.import_module(package_name + '.srv')
-    srv_module = getattr(module, service_name)
+    srv_module = getattr(module, srv_name)
     values_dictionary = json.loads(values)
 
     rclpy.init()
 
-    node = rclpy.create_node('requester_%s_%s' % (package_name, service_name))
+    node = rclpy.create_node('requester_%s_%s' % (package_name, srv_name))
 
     cli = node.create_client(srv_module, service_name)
 
@@ -67,10 +80,18 @@ def requester(service_type, service_name, values):
         field_type = type(getattr(request, field_name))
         setattr(request, field_name, field_type(field_value))
 
-    print('requester: making request: %r\n' % request)
-    cli.call(request)
-    cli.wait_for_future()
-    print('response:\n%r\n' % cli.response)
+    while True:
+        print('requester: making request: %r\n' % request)
+        last_call = time.time()
+        cli.call(request)
+        cli.wait_for_future()
+        if cli.response is not None:
+            print('response:\n%r\n' % cli.response)
+        if loop is None or not rclpy.ok():
+            break
+        time_until_next_period = (last_call + loop) - time.time()
+        if time_until_next_period > 0:
+            time.sleep(time_until_next_period)
 
-    node.shutdown()
-    rclpy.shutdown()
+    node.destroy_node()
+    rclpy.try_shutdown()  # avoid duplicate shutdown from shutdown within cli.wait_for_future()
