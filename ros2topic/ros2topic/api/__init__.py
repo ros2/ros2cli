c35a841f7d3aacb6272b37772bbbf7744f28c937
William Woodall <william+github@osrfoundation.org>
2017-06-17 01:30:59 -0700

    revert no_demangle option until it can be fixed (#9)

--- ros2topic/ros2topic/api/__init__.py
+++ ros2topic/ros2topic/api/__init__.py
@@ -16,8 +16,8 @@ from rclpy.topic_or_service_is_hidden import topic_or_service_is_hidden
 from ros2cli.node.strategy import NodeStrategy
 
 
-def get_topic_names_and_types(*, node, include_hidden_topics=False, no_demangle=False):
-    topic_names_and_types = node.get_topic_names_and_types(no_demangle=no_demangle)
+def get_topic_names_and_types(*, node, include_hidden_topics=False):
+    topic_names_and_types = node.get_topic_names_and_types()
     if not include_hidden_topics:
         topic_names_and_types = [
             (n, t) for (n, t) in topic_names_and_types
