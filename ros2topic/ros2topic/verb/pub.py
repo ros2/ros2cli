f1f5485242a35ce371feb50448abd75db63340c5
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2017-06-27 13:07:21 -0700

    fix population of recursive message fields

--- ros2topic/ros2topic/verb/pub.py
+++ ros2topic/ros2topic/verb/pub.py
@@ -44,7 +44,15 @@ class PubVerb(VerbExtension):
 
 
 def main(args):
-    publisher(args.message_type, args.topic_name, args.values)
+    return publisher(args.message_type, args.topic_name, args.values)
+
+
+class SetFieldError(Exception):
+
+    def __init__(self, field_name, exception):
+        super(SetFieldError, self).__init__()
+        self.field_name = field_name
+        self.exception = exception
 
 
 def publisher(message_type, topic_name, values):
@@ -64,9 +72,11 @@ def publisher(message_type, topic_name, values):
     pub = node.create_publisher(msg_module, topic_name)
 
     msg = msg_module()
-    for field_name, field_value in values_dictionary.items():
-        field_type = type(getattr(msg, field_name))
-        setattr(msg, field_name, field_type(field_value))
+    try:
+        set_msg_fields(msg, values_dictionary)
+    except SetFieldError as e:
+        return "Failed to populate field '{e.field_name}': {e.exception}" \
+            .format_map(locals())
 
     print('publisher: beginning loop')
     while rclpy.ok():
@@ -74,3 +84,22 @@ def publisher(message_type, topic_name, values):
         print('publishing %r\n' % msg)
         time.sleep(1)
     rclpy.shutdown()
+
+
+def set_msg_fields(msg, values):
+    for field_name, field_value in values.items():
+        field_type = type(getattr(msg, field_name))
+        try:
+            value = field_type(field_value)
+        except TypeError:
+            value = field_type()
+            try:
+                set_msg_fields(value, field_value)
+            except SetFieldError as e:
+                raise SetFieldError(
+                    '{field_name}.{e.field_name}'.format_map(locals()),
+                    e.exception)
+        try:
+            setattr(msg, field_name, value)
+        except Exception as e:
+            raise SetFieldError(field_name, e)
