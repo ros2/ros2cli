afee515643af3a42be87cf6a7b3b6613290fc1f5
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2017-09-12 11:01:56 -0700

    fix SIGINT handling in ros2 run

--- ros2run/ros2run/api/__init__.py
+++ ros2run/ros2run/api/__init__.py
@@ -52,8 +52,16 @@ def run_executable(*, path, argv, prefix=None):
     cmd = [path] + argv
     if prefix is not None:
         cmd = prefix + cmd
-    completed_process = subprocess.run(cmd)
-    return completed_process.returncode
+
+    process = subprocess.Popen(cmd)
+    while process.returncode is None:
+        try:
+            process.communicate()
+        except KeyboardInterrupt:
+            # the subprocess will also receive the signal and should shut down
+            # therefore we continue here until the process has finished
+            pass
+    return process.returncode
 
 
 class ExecutableNameCompleter:
