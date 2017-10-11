2409d8b230744608c40daf72189ac3e894435b60
Chris Lalancette <clalancette@gmail.com>
2017-10-11 15:40:18 -0400

    Make sure to check errors when expanding the topic name. (#58)
    * Make sure to check errors when expanding the topic name.
    
    We need to catch ValueErrors when actually doing the expansion,
    then InvalidTopicNameException when doing the validation.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>
    
    * Switch to using the string from the original exception.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- ros2topic/ros2topic/verb/echo.py
+++ ros2topic/ros2topic/verb/echo.py
@@ -99,8 +99,14 @@ def represent_ordereddict(dumper, data):
 def subscriber(node, topic_name, message_type, callback):
     if message_type is None:
         topic_names_and_types = get_topic_names_and_types(node=node)
-        expanded_name = expand_topic_name(topic_name, node.get_name(), node.get_namespace())
-        validate_full_topic_name(expanded_name)
+        try:
+            expanded_name = expand_topic_name(topic_name, node.get_name(), node.get_namespace())
+        except ValueError as e:
+            raise RuntimeError(e)
+        try:
+            validate_full_topic_name(expanded_name)
+        except rclpy.exceptions.InvalidTopicNameException as e:
+            raise RuntimeError(e)
         for n, t in topic_names_and_types:
             if n == expanded_name:
                 if len(t) > 1:
