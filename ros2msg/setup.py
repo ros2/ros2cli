531c8fb84e95316b957aba1b252925338d68187c
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-03-28 11:27:41 -0700

    0.4.1

--- ros2msg/setup.py
+++ ros2msg/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ros2msg',
-    version='0.4.0',
+    version='0.4.1',
     packages=find_packages(exclude=['test']),
     install_requires=['ros2cli'],
     author='Dirk Thomas',
