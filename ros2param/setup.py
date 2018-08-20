4dbd63b67fe5f06aa0845024cb126f9433c0cf22
Mikael Arguedas <mikael@osrfoundation.org>
2018-08-20 14:12:32 -0700

    0.5.4

--- ros2param/setup.py
+++ ros2param/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ros2param',
-    version='0.5.3',
+    version='0.5.4',
     packages=find_packages(exclude=['test']),
     install_requires=['ros2cli'],
     zip_safe=True,
