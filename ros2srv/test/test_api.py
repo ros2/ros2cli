b6ed1a1e85be7a91a30e94c3d804223920b32b35
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-12-12 16:24:09 -0800

    use collections.abc.Iterable

--- ros2srv/test/test_api.py
+++ ros2srv/test/test_api.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from collections import Iterable
+from collections.abc import Iterable
 import os
 
 from ros2srv.api import get_all_service_types
