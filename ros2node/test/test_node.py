e1215c7c5fb3205d4c5ddf0883e7536d25b369a6
ivanpauno <ivanpauno@ekumenlabs.com>
2019-03-13 11:49:51 -0300

    Added test
    Signed-off-by: ivanpauno <ivanpauno@ekumenlabs.com>

---
+++ ros2node/test/test_node.py
@@ -0,0 +1,28 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ros2node.api import parse_node_name
+
+
+def test_parse_node_name():
+    """Test parse_node_name api function."""
+    # The input of parse_node_name is the full_name without the initial '/'
+    name = parse_node_name('talker')
+    assert name.full_name == '/talker'
+    assert name.namespace == '/'
+    assert name.name == 'talker'
+    name = parse_node_name('ns/talker')
+    assert name.full_name == '/ns/talker'
+    assert name.namespace == '/ns'
+    assert name.name == 'talker'
