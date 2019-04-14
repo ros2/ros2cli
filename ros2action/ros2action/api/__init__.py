a463030d6890e988eedad3b26636bd71c154edab
Jacob Perron <jacob@openrobotics.org>
2019-04-14 07:31:32 -0700

    Add Action CLI (#214)
    * Add ros2action package
    
    Contains ros2cli command 'action' with verbs: list and show.
    The list verb lists action names for any running action servers and action clients.
    The show verb prints the definition for a given action type.
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Add 'info' verb to action command
    
    Prints a list of node names that have an action client or server for a given action name.
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Use None as argument to test node
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Add TODOs to move action query functions to rclpy (and rcl_action)
    
    The tool shouldn't need to know details about the implementation of actions.
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Add dependency to rclpy
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Add 'send_goal' verb to action command
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Migrate message utility functions to rosidl_runtime_py
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Make use of rclpy functions
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Fix lint
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Fix tests
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Fix test
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Add autocompletion to verbs
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Update year
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Expand and validate action name
    
    This also has the side-effect of making the forward slash optional for the action name.
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Print goal ID when sendind a goal
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Cancel goal on SIGINT
    
    Wrapped send goal logic in try-finally clause.
    This ensures that any active goal will be canceled before the CLI command terminates and also ensure that the ROS node is shutdown.
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Fix typos
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Change maintainer
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Move try-except to verb
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Catch expected exceptions only
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

---
+++ ros2action/ros2action/api/__init__.py
@@ -0,0 +1,141 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python import get_resource
+from ament_index_python import get_resources
+from ament_index_python import has_resource
+
+import rclpy.action
+from rclpy.expand_topic_name import expand_topic_name
+from rclpy.validate_full_topic_name import validate_full_topic_name
+from ros2cli.node.direct import DirectNode
+
+
+def _is_action_status_topic(topic_name, action_name):
+    return action_name + '/_action/status' == topic_name
+
+
+def get_action_clients_and_servers(*, node, action_name):
+    action_clients = []
+    action_servers = []
+
+    expanded_name = expand_topic_name(action_name, node.get_name(), node.get_namespace())
+    validate_full_topic_name(expanded_name)
+
+    node_names_and_ns = node.get_node_names_and_namespaces()
+    for node_name, node_ns in node_names_and_ns:
+        # Construct fully qualified name
+        node_fqn = '/'.join(node_ns) + node_name
+
+        # Get any action clients associated with the node
+        client_names_and_types = rclpy.action.get_action_client_names_and_types_by_node(
+            node,
+            node_name,
+            node_ns,
+        )
+        for client_name, client_types in client_names_and_types:
+            if client_name == expanded_name:
+                action_clients.append((node_fqn, client_types))
+
+        # Get any action servers associated with the node
+        server_names_and_types = rclpy.action.get_action_server_names_and_types_by_node(
+            node,
+            node_name,
+            node_ns,
+        )
+        for server_name, server_types in server_names_and_types:
+            if server_name == expanded_name:
+                action_servers.append((node_fqn, server_types))
+
+    return (action_clients, action_servers)
+
+
+def get_action_names_and_types(*, node):
+    return rclpy.action.get_action_names_and_types(node)
+
+
+def get_action_names(*, node):
+    action_names_and_types = get_action_names_and_types(node=node)
+    return [n for (n, t) in action_names_and_types]
+
+
+def get_action_types(package_name):
+    if not has_resource('packages', package_name):
+        raise LookupError('Unknown package name')
+    try:
+        content, _ = get_resource('rosidl_interfaces', package_name)
+    except LookupError:
+        return []
+    interface_names = content.splitlines()
+    # TODO(jacobperron) this logic should come from a rosidl related package
+    # Only return actions in action folder
+    return list(sorted({
+        n[7:-7]
+        for n in interface_names
+        if n.startswith('action/') and n[-7:] in ('.idl', '.action')}))
+
+
+def get_all_action_types():
+    all_action_types = {}
+    for package_name in get_resources('rosidl_interfaces'):
+        action_types = get_action_types(package_name)
+        if action_types:
+            all_action_types[package_name] = action_types
+    return all_action_types
+
+
+def get_action_path(package_name, action_name):
+    action_types = get_action_types(package_name)
+    if action_name not in action_types:
+        raise LookupError('Unknown action type')
+    prefix_path = has_resource('packages', package_name)
+    # TODO(jacobperron) this logic should come from a rosidl related package
+    return os.path.join(prefix_path, 'share', package_name, 'action', action_name + '.action')
+
+
+def action_name_completer(prefix, parsed_args, **kwargs):
+    """Callable returning a list of action names."""
+    with DirectNode(parsed_args) as node:
+        return get_action_names(node=node)
+
+
+def action_type_completer(**kwargs):
+    """Callable returning a list of action types."""
+    action_types = []
+    for package_name, action_names in get_all_action_types().items():
+        for action_name in action_names:
+            action_types.append(
+                '{package_name}/{action_name}'.format_map(locals()))
+    return action_types
+
+
+class ActionTypeCompleter:
+    """Callable returning a list of action types."""
+
+    def __init__(self, *, action_name_key=None):
+        self.action_name_key = action_name_key
+
+    def __call__(self, prefix, parsed_args, **kwargs):
+        if self.action_name_key is None:
+            return action_type_completer()
+
+        action_name = getattr(parsed_args, self.action_name_key)
+        with DirectNode(parsed_args) as node:
+            names_and_types = get_action_names_and_types(node=node)
+            for n, t in names_and_types:
+                if n == action_name:
+                    return t
+        return []
---
+++ ros2action/ros2action/verb/send_goal.py
@@ -0,0 +1,163 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import importlib
+
+from action_msgs.msg import GoalStatus
+import rclpy
+from rclpy.action import ActionClient
+from ros2action.api import action_name_completer
+from ros2action.api import ActionTypeCompleter
+from ros2action.verb import VerbExtension
+from ros2cli.node import NODE_NAME_PREFIX
+from rosidl_runtime_py import message_to_yaml
+from rosidl_runtime_py import set_message_fields
+
+import yaml
+
+
+class SendGoalVerb(VerbExtension):
+    """Send an action goal."""
+
+    def add_arguments(self, parser, cli_name):
+        arg = parser.add_argument(
+            'action_name',
+            help="Name of the ROS action (e.g. '/fibonacci')")
+        arg.completer = action_name_completer
+        arg = parser.add_argument(
+            'action_type',
+            help="Type of the ROS action (e.g. 'example_interfaces/Fibonacci')")
+        arg.completer = ActionTypeCompleter(action_name_key='action_name')
+        parser.add_argument(
+            'goal',
+            help="Goal request values in YAML format (e.g. '{order: 10}')")
+        parser.add_argument(
+            '-f', '--feedback', action='store_true',
+            help='Echo feedback messages for the goal')
+
+    def main(self, *, args):
+        feedback_callback = None
+        if args.feedback:
+            feedback_callback = _feedback_callback
+        return send_goal(args.action_name, args.action_type, args.goal, feedback_callback)
+
+
+def _goal_status_to_string(status):
+    if GoalStatus.STATUS_ACCEPTED == status:
+        return 'ACCEPTED'
+    elif GoalStatus.STATUS_EXECUTING == status:
+        return 'EXECUTING'
+    elif GoalStatus.STATUS_CANCELING == status:
+        return 'CANCELING'
+    elif GoalStatus.STATUS_SUCCEEDED == status:
+        return 'SUCCEEDED'
+    elif GoalStatus.STATUS_CANCELED == status:
+        return 'CANCELED'
+    elif GoalStatus.STATUS_ABORTED == status:
+        return 'ABORTED'
+    else:
+        return 'UNKNOWN'
+
+
+def _feedback_callback(feedback):
+    print('Feedback:\n    {}'.format(message_to_yaml(feedback.feedback, None)))
+
+
+def send_goal(action_name, action_type, goal_values, feedback_callback):
+    goal_handle = None
+    node = None
+    action_client = None
+    try:
+        # TODO(jacobperron): This logic should come from a rosidl related package
+        package_name, action_type = action_type.split('/', 2)
+        if not package_name or not action_type:
+            raise RuntimeError('The passed action type is invalid')
+
+        module = importlib.import_module(package_name + '.action')
+        action_module = getattr(module, action_type)
+        goal_dict = yaml.load(goal_values)
+
+        rclpy.init()
+
+        node_name = NODE_NAME_PREFIX + '_send_goal_{}_{}'.format(package_name, action_type)
+        node = rclpy.create_node(node_name)
+
+        action_client = ActionClient(node, action_module, action_name)
+
+        goal = action_module.Goal()
+
+        try:
+            set_message_fields(goal, goal_dict)
+        except Exception as ex:
+            return 'Failed to populate message fields: {!r}'.format(ex)
+
+        print('Waiting for an action server to become available...')
+        action_client.wait_for_server()
+
+        print('Sending goal:\n     {}'.format(message_to_yaml(goal, None)))
+        goal_future = action_client.send_goal_async(goal, feedback_callback)
+        rclpy.spin_until_future_complete(node, goal_future)
+
+        goal_handle = goal_future.result()
+
+        if goal_handle is None:
+            raise RuntimeError(
+                'Exeception while sending goal: {!r}'.format(goal_future.exception()))
+
+        if not goal_handle.accepted:
+            print('Goal was rejected.')
+            return
+
+        print('Goal accepted with ID: {}\n'.format(bytes(goal_handle.goal_id.uuid).hex()))
+
+        result_future = goal_handle.get_result_async()
+        rclpy.spin_until_future_complete(node, result_future)
+
+        result = result_future.result()
+
+        if result is None:
+            raise RuntimeError(
+                'Exeception while getting result: {!r}'.format(result_future.exception()))
+
+        print('Result:\n    {}'.format(message_to_yaml(result.result, None)))
+        print('Goal finished with status: {}'.format(_goal_status_to_string(result.status)))
+    finally:
+        # Cancel the goal if it's still active
+        if (goal_handle is not None and
+            (GoalStatus.STATUS_ACCEPTED == goal_handle.status or
+             GoalStatus.STATUS_EXECUTING == goal_handle.status)):
+            print('Canceling goal...')
+            cancel_future = goal_handle.cancel_goal_async()
+            rclpy.spin_until_future_complete(node, cancel_future)
+
+            cancel_response = cancel_future.result()
+
+            if cancel_response is None:
+                raise RuntimeError(
+                    'Exeception while canceling goal: {!r}'.format(cancel_future.exception()))
+
+            if len(cancel_response.goals_canceling) == 0:
+                raise RuntimeError('Failed to cancel goal')
+            if len(cancel_response.goals_canceling) > 1:
+                raise RuntimeError('More than one goal canceled')
+            if cancel_response.goals_canceling[0].goal_id != goal_handle.goal_id:
+                raise RuntimeError('Canceled goal with incorrect goal ID')
+            print('Goal canceled.')
+
+        if action_client is not None:
+            action_client.destroy()
+        if node is not None:
+            node.destroy_node()
+        if rclpy.ok():
+            rclpy.shutdown()
