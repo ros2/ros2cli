3e8f788ec7c7c19c9e34196ee07a8b1b90ef8532
Siddharth Kucheria <kucheria@usc.edu>
2019-06-20 17:44:48 -0700

    initial commit. most ros2interface verbs are done

---
+++ ros2interface/ros2interface/verb/list.py
@@ -0,0 +1,54 @@
+# Copyright 2017 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ros2interface.api import get_all_message_types
+from ros2interface.verb import VerbExtension
+from ros2interface.api import get_all_service_types
+from ros2cli.node.strategy import add_arguments
+
+def printMessages():
+    print('Messages: ')
+    message_types = get_all_message_types()
+    for package_name in sorted(message_types.keys()):
+        for message_name in sorted(message_types[package_name]):
+            print('{package_name}/msg/{message_name}'.format_map(locals()))
+
+def printServices():
+    print('Services: ')
+    service_types = get_all_service_types()
+    for package_name in sorted(service_types.keys()):
+        for service_name in sorted(service_types[package_name]):
+            print('{package_name}/srv/{service_name}'.format_map(locals()))
+
+
+class ListVerb(VerbExtension):
+    """ Description """
+
+    def add_arguments(self, parser, cli_name):
+        parser.add_argument('-m', '--only-msgs', action='count',
+               help='Print out only the message types')
+
+        parser.add_argument('-s', '--only-srvs', action='count',
+               help='Print out only the srvs types')
+
+    def main(self, *, args):
+        if(args.only_msgs == 1):
+            printMessages()
+            return
+        if(args.only_srvs == 1):
+            printServices()
+            return
+        printMessages()
+        printServices()
+    
