f70596ea8ea8acd7d3195c3ac80b7c87cb7deef7
Siddharth Kucheria <kucheria@usc.edu>
2019-06-27 09:43:17 -0700

    reorganized tests for clarity

--- ros2interface/test/test_api.py
+++ ros2interface/test/test_api.py
@@ -20,23 +20,7 @@ from ros2interface.api import get_interface
 from ros2interface.api import get_interface_path
 
 
-def test_api():
-    # testing get_all_interface_packages with 'packages' verb
-    packages_cmd = ['ros2', 'interface', 'packages', '-a']
-    packages_result = subprocess.run(packages_cmd, stdout=subprocess.PIPE, check=True)
-    package_names = packages_result.stdout.decode().splitlines()
-    assert len(package_names) == 4
-
-    # check all packages being found
-    interface_packages = get_all_interface_packages()
-    assert len(interface_packages) == 26
-
-    # check known package name
-    get_interface('std_msgs')
-    path = ['std_msgs', 'msg', 'Empty.msg']
-    interface_path = get_interface_path(path)
-    assert os.path.isfile(interface_path)
-
+def test_get_interface():
     # check not existing package name
     try:
         get_interface('_not_existing_package_name')
@@ -47,3 +31,23 @@ def test_api():
     # check interface getter from package name
     interface_names = get_interface('std_srvs')
     assert len(interface_names) == 3
+
+
+def test_get_interface_path():
+    # check known package name
+    get_interface('std_msgs')
+    path = ['std_msgs', 'msg', 'Empty.msg']
+    interface_path = get_interface_path(path)
+    assert os.path.isfile(interface_path)
+
+
+def test_get_all_interface_pacakges():
+    # testing get_all_interface_packages with 'packages' verb
+    packages_cmd = ['ros2', 'interface', 'packages', '-a']
+    packages_result = subprocess.run(packages_cmd, stdout=subprocess.PIPE, check=True)
+    package_names = packages_result.stdout.decode().splitlines()
+    assert len(package_names) == 4
+
+    # check all packages being found
+    interface_packages = get_all_interface_packages()
+    assert len(interface_packages) == 26
