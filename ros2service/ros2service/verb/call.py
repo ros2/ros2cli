a345e63f43a7539783dfaa7a17862d53c1bce0e6
artivis <jeremie.deray@canonical.com>
2019-07-17 10:03:57 -0400

    add service call prototype completer
    Signed-off-by: artivis <jeremie.deray@canonical.com>

--- ros2service/ros2service/verb/call.py
+++ ros2service/ros2service/verb/call.py
@@ -18,6 +18,7 @@ import time
 import rclpy
 from ros2cli.node import NODE_NAME_PREFIX
 from ros2service.api import ServiceNameCompleter
+from ros2service.api import ServicePrototypeCompleter
 from ros2service.api import ServiceTypeCompleter
 from ros2service.verb import VerbExtension
 from rosidl_runtime_py import set_message_fields
@@ -38,11 +39,13 @@ class CallVerb(VerbExtension):
             help="Type of the ROS service (e.g. 'std_srvs/srv/Empty')")
         arg.completer = ServiceTypeCompleter(
             service_name_key='service_name')
-        parser.add_argument(
+        arg = parser.add_argument(
             'values', nargs='?', default='{}',
             help='Values to fill the service request with in YAML format ' +
                  '(e.g. "{a: 1, b: 2}"), ' +
                  'otherwise the service request will be published with default values')
+        arg.completer = ServicePrototypeCompleter(
+            service_type_key='service_type')
         parser.add_argument(
             '-r', '--rate', metavar='N', type=float,
             help='Repeat the call at a specific rate in Hz')
