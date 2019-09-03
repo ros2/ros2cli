8e03d757dcc050dfcd7b59bae491b6c6100968cd
James Pace <jpace121@gmail.com>
2019-09-03 19:25:20 -0400

    Abort when package name = test.
    Signed-off-by: James Pace <jpace121@gmail.com>

--- ros2pkg/ros2pkg/verb/create.py
+++ ros2pkg/ros2pkg/verb/create.py
@@ -125,6 +125,14 @@ class CreateVerb(VerbExtension):
             test_dependencies = ['ament_copyright', 'ament_flake8', 'ament_pep257',
                                  'python3-pytest']
 
+        if args.build_type == 'ament_python':
+            if args.package_name == 'test':
+                # If the package name is 'test', there will be a conflict between
+                # the directory the source code for the package goes in and the
+                # directory the tests for the package go in.
+                return '\nAborted!\nament_python packages can not be named "test". \n' + \
+                    'Please choose a different package name.'
+
         package = Package(
             package_format=args.package_format,
             name=args.package_name,
