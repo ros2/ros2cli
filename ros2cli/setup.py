759315fbda2009991119d7c8b53a9502e0e179d0
Chris Lalancette <clalancette@openrobotics.org>
2019-09-26 20:19:09 +0000

    0.8.0

--- ros2cli/setup.py
+++ ros2cli/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ros2cli',
-    version='0.7.4',
+    version='0.8.0',
     packages=find_packages(exclude=['test']),
     extras_require={
         'completion': ['argcomplete'],
