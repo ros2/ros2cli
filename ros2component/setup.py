a997edcd05f46093b093dbe37cb49b6814c0258d
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-10-10 13:04:57 -0700

    install package manifest (#330) (#359)
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>
    (cherry picked from commit bc32fb1deb5767fbafb360d53fbac097f0f7d538)

--- ros2component/setup.py
+++ ros2component/setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages
 from setuptools import setup
 
+package_name = 'ros2component'
+
 setup(
-    name='ros2component',
+    name=package_name,
     version='0.6.3',
     packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['ros2cli'],
     zip_safe=True,
     author='Michel Hidalgo',
