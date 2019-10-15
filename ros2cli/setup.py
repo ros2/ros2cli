d02fd5f342c37ea5c778f0edfcc925e4e4b2d173
Steven! Ragnarök <steven@nuclearsandwich.com>
2019-10-14 20:24:30 -0400

    0.7.6

--- ros2cli/setup.py
+++ ros2cli/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ros2cli',
-    version='0.7.5',
+    version='0.7.6',
     packages=find_packages(exclude=['test']),
     extras_require={
         'completion': ['argcomplete'],
