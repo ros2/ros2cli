b61a6052c88ef3d833f0295551f9911ec672c444
evshary <evshary@gmail.com>
2019-10-20 11:47:26 +0800

    Use NodeStrategy instead of DirectNode for showing topic list.
    We also need to register functions for NodeStrategy: count_publishers and
    count_subscribers.
    
    Signed-off-by: evshary <evshary@gmail.com>

--- ros2topic/ros2topic/verb/list.py
+++ ros2topic/ros2topic/verb/list.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ros2cli.node.direct import DirectNode
 from ros2cli.node.strategy import add_arguments as add_strategy_node_arguments
 from ros2cli.node.strategy import NodeStrategy
 from ros2topic.api import get_topic_names_and_types
@@ -53,7 +52,7 @@ class ListVerb(VerbExtension):
 
     def main(self, *, args):
         topic_info = []
-        with DirectNode(args) as node:
+        with NodeStrategy(args) as node:
             topic_names_and_types = get_topic_names_and_types(
                 node=node,
                 include_hidden_topics=args.include_hidden_topics)
