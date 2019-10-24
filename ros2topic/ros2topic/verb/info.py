adc7573d5a6eac475b2ae6825828da2ae66b5313
Jaison Titus <jaisontj92@gmail.com>
2019-10-24 15:06:07 -0700

    Enhanced ros2 topic info to display node name, node namespace, topic type, gid and qos profile of the publishers and subscribers.
    Signed-off-by: Jaison Titus <jaisontj92@gmail.com>

--- ros2topic/ros2topic/verb/info.py
+++ ros2topic/ros2topic/verb/info.py
@@ -12,11 +12,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from rclpy.qos import QoSDurabilityPolicy
+from rclpy.qos import QoSLivelinessPolicy
+from rclpy.qos import QoSReliabilityPolicy
+
 from ros2cli.node.direct import DirectNode
 from ros2topic.api import get_topic_names_and_types
 from ros2topic.api import TopicNameCompleter
 from ros2topic.verb import VerbExtension
 
+def print_topic_info(info):
+    print('\nNode Name: %s' % info['node_name'])
+    print('Node Namespace: %s' % info['node_namespace'])
+    print('Topic Type: %s' % info['topic_type'])
+    print('GID: %s' % info['gid'])
+    print('QoS Profile:')
+    qos_profile = info['qos_profile']
+    print('  Reliability: %s' % QoSReliabilityPolicy(qos_profile['reliability']).name)
+    print('  Durability: %s' % QoSDurabilityPolicy(qos_profile['durability']).name)
+    print('  Lifespan: %d nanoseconds' % qos_profile['lifespan'].nanoseconds)
+    print('  Deadline: %d nanoseconds' % qos_profile['deadline'].nanoseconds)
+    print('  Liveliness: %s' % QoSLivelinessPolicy(qos_profile['liveliness']).name)
+    print('  Liveliness Lease Duration: %d nanoseconds' %
+          qos_profile['liveliness_lease_duration'].nanoseconds)
 
 class InfoVerb(VerbExtension):
     """Print information about a topic."""
@@ -41,5 +59,9 @@ class InfoVerb(VerbExtension):
                 return "Unknown topic '%s'" % topic_name
             type_str = topic_types[0] if len(topic_types) == 1 else topic_types
             print('Type: %s' % type_str)
-            print('Publisher count: %d' % node.count_publishers(topic_name))
-            print('Subscriber count: %d' % node.count_subscribers(topic_name))
+            print('\nPublisher count    : %d' % node.count_publishers(topic_name))
+            for publisher_info in node.get_publishers_info_by_topic(topic_name):
+                print_topic_info(publisher_info)
+            print('\nSubscription count : %d' % node.count_subscribers(topic_name))
+            for subscription_info in node.get_subscriptions_info_by_topic(topic_name):
+                print_topic_info(subscription_info)
