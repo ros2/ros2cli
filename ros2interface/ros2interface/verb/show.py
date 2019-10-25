95d4be5acfd49f22642e5fa629819428182e7971
Nursharmin Ramli <nursharminramli@gmail.com>
2019-10-26 02:36:46 +0800

    Allow stdin input for 'ros2 interface show'
    Signed-off-by: Nursharmin Ramli <nursharminramli@gmail.com>

--- ros2interface/ros2interface/verb/show.py
+++ ros2interface/ros2interface/verb/show.py
@@ -12,18 +12,44 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import sys
+import argparse
+import textwrap
+
 from ros2interface.api import type_completer
 from ros2interface.verb import VerbExtension
 from rosidl_runtime_py import get_interface_path
+
+
+class ReadStdinPipe(argparse.Action):
+    """Get argument from stdin pipe."""
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        if values == '-':
+            if not sys.stdin.isatty():
+                setattr(namespace, self.dest, sys.stdin.readline().strip())
+            else:
+                parser.error('expected stdin pipe')
+        else:
+            setattr(namespace, self.dest, values)
 
 
 class ShowVerb(VerbExtension):
     """Output the interface definition."""
 
     def add_arguments(self, parser, cli_name):
+        parser.formatter_class = argparse.RawTextHelpFormatter
+        parser.epilog = textwrap.dedent("""\
+                additional usage:
+                    show the interface definition of a topic, service or action
+                    (e.g. ros2 topic type /chatter | ros2 interface show -)""")
+
         arg = parser.add_argument(
                 'type',
-                help="Show an interface definition (e.g. 'example_interfaces/msg/String')")
+                action=ReadStdinPipe,
+                help=textwrap.dedent("""\
+                        Show an interface definition (e.g. "std_msgs/msg/String")
+                        Set to "-" to read the interface type from the stdin pipe"""))
         arg.completer = type_completer
 
     def main(self, *, args):
