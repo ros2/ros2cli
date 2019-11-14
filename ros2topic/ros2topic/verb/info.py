1c84126ef555c5a7540b57fe1fd0e783bb4e1f2f
Jaison Titus <jaisontj92@gmail.com>
2019-11-14 11:52:18 -0800

    Added a --verbose flag to display the additional details and error handling for cases when the underlying rmw_implementation may not have implemented the functions.
    Signed-off-by: Jaison Titus <jaisontj92@gmail.com>

--- ros2topic/ros2topic/verb/info.py
+++ ros2topic/ros2topic/verb/info.py
@@ -20,21 +20,29 @@ from ros2cli.node.direct import DirectNode
 from ros2topic.api import get_topic_names_and_types
 from ros2topic.api import TopicNameCompleter
 from ros2topic.verb import VerbExtension
+import logging
+logger = logging.getLogger(__name__)
 
-def print_topic_info(info):
-    print('\nNode Name: %s' % info['node_name'])
-    print('Node Namespace: %s' % info['node_namespace'])
-    print('Topic Type: %s' % info['topic_type'])
-    print('GID: %s' % info['gid'])
-    print('QoS Profile:')
-    qos_profile = info['qos_profile']
-    print('  Reliability: %s' % QoSReliabilityPolicy(qos_profile['reliability']).name)
-    print('  Durability: %s' % QoSDurabilityPolicy(qos_profile['durability']).name)
-    print('  Lifespan: %d nanoseconds' % qos_profile['lifespan'].nanoseconds)
-    print('  Deadline: %d nanoseconds' % qos_profile['deadline'].nanoseconds)
-    print('  Liveliness: %s' % QoSLivelinessPolicy(qos_profile['liveliness']).name)
-    print('  Liveliness Lease Duration: %d nanoseconds' %
-          qos_profile['liveliness_lease_duration'].nanoseconds)
+def print_topic_info(topic_name, get_topic_info_func):
+    try:
+        for info in get_topic_info_func(topic_name):
+            print('\nNode Name: %s' % info['node_name'])
+            print('Node Namespace: %s' % info['node_namespace'])
+            print('Topic Type: %s' % info['topic_type'])
+            print('GID: %s' % info['gid'])
+            print('QoS Profile:')
+            qos_profile = info['qos_profile']
+            print('  Reliability: %s' % QoSReliabilityPolicy(qos_profile['reliability']).name)
+            print('  Durability: %s' % QoSDurabilityPolicy(qos_profile['durability']).name)
+            print('  Lifespan: %d nanoseconds' % qos_profile['lifespan'].nanoseconds)
+            print('  Deadline: %d nanoseconds' % qos_profile['deadline'].nanoseconds)
+            print('  Liveliness: %s' % QoSLivelinessPolicy(qos_profile['liveliness']).name)
+            print('  Liveliness Lease Duration: %d nanoseconds' %
+                  qos_profile['liveliness_lease_duration'].nanoseconds)
+    except NotImplementedError as e:
+        logger.warning(str(e))
+    except Exception as e:
+        logger.error(str(e))
 
 class InfoVerb(VerbExtension):
     """Print information about a topic."""
@@ -43,6 +51,10 @@ class InfoVerb(VerbExtension):
         arg = parser.add_argument(
             'topic_name',
             help="Name of the ROS topic to get info (e.g. '/chatter')")
+        parser.add_argument(
+            '--verbose', '-v', action='store_true',
+            help='Prints detailed information like the Node Name, Node Namespace, Topic Type, '
+                 'GUID and QoS Profile of the publishers and subscribers to this topic')
         arg.completer = TopicNameCompleter(
             include_hidden_topics_key='include_hidden_topics')
 
@@ -60,8 +72,9 @@ class InfoVerb(VerbExtension):
             type_str = topic_types[0] if len(topic_types) == 1 else topic_types
             print('Type: %s' % type_str)
             print('\nPublisher count    : %d' % node.count_publishers(topic_name))
-            for publisher_info in node.get_publishers_info_by_topic(topic_name):
-                print_topic_info(publisher_info)
+            if (args.verbose):
+                print_topic_info(topic_name, node.get_publishers_info_by_topic)
             print('\nSubscription count : %d' % node.count_subscribers(topic_name))
-            for subscription_info in node.get_subscriptions_info_by_topic(topic_name):
-                print_topic_info(subscription_info)
+            if (args.verbose):
+                print_topic_info(topic_name, node.get_subscriptions_info_by_topic)
+
