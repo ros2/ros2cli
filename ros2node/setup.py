c88e3d2a508c3e54e8d7295870abe280589ce2f9
Shane Loretz <sloretz@osrfoundation.org>
2019-11-14 14:12:56 -0800

    0.8.5
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- ros2node/setup.py
+++ ros2node/setup.py
@@ -5,7 +5,7 @@ package_name = 'ros2node'
 
 setup(
     name=package_name,
-    version='0.8.4',
+    version='0.8.5',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
