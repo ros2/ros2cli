3271101576a54f7a21bf9c00481ff56885be6060
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-11-14 10:44:01 -0800

    fix import order
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>

--- ros2interface/ros2interface/verb/show.py
+++ ros2interface/ros2interface/verb/show.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
 import argparse
+import sys
 
 from ros2interface.api import type_completer
 from ros2interface.verb import VerbExtension
