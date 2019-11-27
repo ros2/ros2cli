d8042399c6ff314eb4c81b243ac269d84cd8d769
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-11-27 13:43:20 -0800

    0.7.8
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>

--- ros2cli/setup.py
+++ ros2cli/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ros2cli',
-    version='0.7.7',
+    version='0.7.8',
     packages=find_packages(exclude=['test']),
     extras_require={
         'completion': ['argcomplete'],
--- ros2service/setup.py
+++ ros2service/setup.py
@@ -5,7 +5,7 @@ package_name = 'ros2service'
 
 setup(
     name=package_name,
-    version='0.7.7',
+    version='0.7.8',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
