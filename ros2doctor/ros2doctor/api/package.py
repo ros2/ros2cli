f82c7b7e5988e3ed4bdd32fd673b68fa9931de28
claireyywang <clairewang@openrobotics.org>
2019-12-02 17:48:04 -0800

    add error msg to check
    Signed-off-by: claireyywang <clairewang@openrobotics.org>

--- ros2doctor/ros2doctor/api/package.py
+++ ros2doctor/ros2doctor/api/package.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import os
+from urllib.error import URLError
 
 from ament_index_python import get_packages_with_prefixes
 from catkin_pkg.package import InvalidPackage
@@ -31,7 +32,7 @@ def get_ros_repos_info() -> dict:
     """
     Return repos info using rosdistro API.
 
-    :return: a dictionary containing package name, release, source and status
+    :return: a dictionary contains package name, release, source and status
     """
     distro_name = os.environ.get('ROS_DISTRO')
     distro_name = distro_name.lower()
@@ -46,7 +47,7 @@ def get_packages_versions(distro_repos_info: dict) -> dict:
     Return all package names and corresponding versions in rosdistro.
 
     :param: a dictionary of repo info
-    :return: a dictionary containing package names and versions
+    :return: a dictionary contains package names and versions
     """
     packages_versions = {}
     for _, info in distro_repos_info.items():
@@ -69,30 +70,38 @@ class PackageCheck(DoctorCheck):
     def check(self):
         """Check packages within the directory where command is called."""
         result = Result()
+        local_packages_prefixes = get_packages_with_prefixes()
+        if not local_packages_prefixes:
+            result.add_error('ERROR: local package info is not found.')
         try:
             distro_repos_info = get_ros_repos_info()
             packages_versions = get_packages_versions(distro_repos_info)
-        except (AttributeError, RuntimeError):
-            result.add_error('ERROR: Unable to obtain current distro package information \
-                from rosdistro.')
-            return result
-        local_packages_prefixes = get_packages_with_prefixes()
-        if not local_packages_prefixes or not packages_versions:
-            result.add_error('ERROR: local packages info or distro packages info is empty.')
+            if not packages_versions:
+                result.add_error('ERROR: distro packages info is not found.')
+        except (AttributeError, RuntimeError, URLError):
+            result.add_error('ERROR: Unable to fetch package information from rosdistro.')
+        if result.error != 0:
             return result
         for package_name, package_prefix in local_packages_prefixes.items():
             file_path = os.path.join(package_prefix, 'share', package_name)
-            required_ver = packages_versions.get(package_name)
+            try:
+                required_ver_str = packages_versions.get(package_name)
+                required_ver = version.parse(required_ver_str).base_version
+            except TypeError:
+                result.add_warning(f'{package_name}: No required version found.')
             try:
                 package_obj = parse_package(file_path)
-                local_ver = package_obj.version
+                local_ver_str = package_obj.version
             except (AttributeError, IOError, InvalidPackage):
-                result.add_warning('Unable to parse `%s` package.xml file.' % package_name)
-                local_ver = ''
-            if required_ver and local_ver:
-                if version.parse(local_ver).base_version < version.parse(required_ver).base_version:
-                    result.add_warning('%s local version %s does not match required version %s.'
-                                       % (package_name, package_obj.version, required_ver))
+                result.add_warning(f'Unable to parse {package_name} package.xml file.')
+                local_ver_str = ''
+            try:
+                local_ver = version.parse(local_ver_str).base_version
+            except TypeError:
+                result.add_warning(f'{package_name}: no local version found.')
+            if local_ver < required_ver:
+                result.add_warning('{package_name} is deprecated.'
+                                   f'local: {local_ver} < required: {required_ver}')
         return result
 
 
@@ -105,22 +114,22 @@ class PackageReport(DoctorReport):
     def report(self):
         """Report packages within the directory where command is called."""
         report = Report('PACKAGE VERSIONS')
+        local_package_prefixes = get_packages_with_prefixes()
         try:
             distro_repos_info = get_ros_repos_info()
             packages_versions = get_packages_versions(distro_repos_info)
-        except (AttributeError, RuntimeError):
+        except:
             return report
-        local_package_prefixes = get_packages_with_prefixes()
         if local_package_prefixes and packages_versions:
-            for package_name, package_prefix in local_package_prefixes.items():
-                required_ver = packages_versions.get(package_name)
-                file_path = os.path.join(package_prefix, 'share', package_name)
+            for name, prefix in local_package_prefixes.items():
+                required_ver = packages_versions.get(name)
+                file_path = os.path.join(prefix, 'share', name)
                 try:
                     package_obj = parse_package(file_path)
                     local_ver = package_obj.version
-                except (IOError, InvalidPackage):
+                except:
                     local_ver = ''
-                report.add_to_report(package_name, 'required=' +
-                                     (required_ver if required_ver else '') +
-                                     ', local=' + local_ver)
+                report.add_to_report(name, 'required=' +
+                                    (required_ver if required_ver else '') +
+                                    ', local=' + local_ver)
         return report
