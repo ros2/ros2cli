8e9ed29b6bd980c84c56ddf710a0ada61d3cee1e
Jacob Perron <jacob@openrobotics.org>
2020-01-17 15:03:45 -0800

    0.8.7

--- ros2interface/setup.py
+++ ros2interface/setup.py
@@ -5,7 +5,7 @@ package_name = 'ros2interface'
 
 setup(
     name=package_name,
-    version='0.8.6',
+    version='0.8.7',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
