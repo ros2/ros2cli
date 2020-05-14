bd79e43d1273b3055cb2b431821026b44c696832
Jacob Perron <jacob@openrobotics.org>
2020-05-13 20:13:11 -0700

    0.9.3

--- ros2lifecycle/setup.py
+++ ros2lifecycle/setup.py
@@ -5,7 +5,7 @@ package_name = 'ros2lifecycle'
 
 setup(
     name=package_name,
-    version='0.9.2',
+    version='0.9.3',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
