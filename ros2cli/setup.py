29f58fa0467acafe6ace21c7102ee3ef21f8d4f2
Jacob Perron <jacob@openrobotics.org>
2020-06-01 10:59:56 -0700

    0.9.5

--- ros2cli/setup.py
+++ ros2cli/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ros2cli',
-    version='0.9.4',
+    version='0.9.5',
     packages=find_packages(exclude=['test']),
     extras_require={
         'completion': ['argcomplete'],
