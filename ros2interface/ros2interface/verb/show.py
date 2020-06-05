f7bebe0f88e811104271758f038cef8abb5285d9
Audrow Nash <audrow.nash@gmail.com>
2020-06-05 10:23:36 -0700

    [ros2interface] show "expanded" message definition (#524)
    * Expand interfaces with nesting in interface show
    
    Signed-off-by: Audrow <audrow.nash@gmail.com>
    
    * Make show interface not print anything on empty lines
    
    Signed-off-by: Audrow <audrow.nash@gmail.com>
    
    * Test nesting interface + update tests that used nested interfaces
    
    Signed-off-by: Audrow <audrow.nash@gmail.com>
    
    * Minor refactoring
    
    Signed-off-by: Audrow <audrow.nash@gmail.com>
    
    * Support arrays of expandable types
    
    Signed-off-by: Audrow <audrow.nash@gmail.com>
    
    * Yield file lines and use stack to organize nesting
    
    Signed-off-by: Audrow <audrow.nash@gmail.com>
    
    * Make member variable names and error text more consistent
    
    Signed-off-by: Audrow <audrow.nash@gmail.com>
    
    * Use str function to make TextLineInterface usage clearer
    
    Signed-off-by: Audrow <audrow.nash@gmail.com>
    
    * Simplify generator to get interface text lines
    
    Signed-off-by: Audrow <audrow.nash@gmail.com>
    
    * Reformat to pass flake-8 tests
    
    Signed-off-by: Audrow <audrow.nash@gmail.com>

--- ros2interface/ros2interface/verb/show.py
+++ ros2interface/ros2interface/verb/show.py
@@ -14,10 +14,103 @@
 
 import argparse
 import sys
+import typing
 
 from ros2interface.api import type_completer
 from ros2interface.verb import VerbExtension
+from rosidl_adapter.parser import \
+    ACTION_REQUEST_RESPONSE_SEPARATOR, \
+    Field, \
+    MessageSpecification, \
+    parse_message_string, \
+    SERVICE_REQUEST_RESPONSE_SEPARATOR
 from rosidl_runtime_py import get_interface_path
+
+
+class InterfaceTextLine:
+    """
+    A convenience class for a single text line in an interface file.
+
+    This class uses the class MessageSpecification to parse a single line
+    from an interface file and makes the type of the message accessible so
+    that it can be used to get the file path to nested messages.
+    """
+
+    def __init__(
+            self,
+            pkg_name: str,
+            msg_name: str,
+            line_text: str,
+    ):
+        if line_text in (SERVICE_REQUEST_RESPONSE_SEPARATOR, ACTION_REQUEST_RESPONSE_SEPARATOR):
+            msg_spec = None
+        else:
+            msg_spec = parse_message_string(
+                pkg_name=pkg_name,
+                msg_name=msg_name,
+                message_string=line_text,
+            )
+            if len(msg_spec.fields) > 1:
+                raise ValueError("'line_text' must be only one line")
+        self._msg_spec: typing.Optional[MessageSpecification] = msg_spec
+        self._raw_line_text = line_text
+
+    def __str__(self) -> str:
+        return self._raw_line_text
+
+    @property
+    def nested_type(self) -> typing.Optional[str]:
+        if self._field and self._is_nested():
+            interface_type: str = str(self._field.type)
+            if self._field.type.is_array:
+                interface_type = interface_type[:interface_type.find('[')]
+            return interface_type.replace('/', '/msg/')
+
+    @property
+    def _field(self) -> typing.Optional[Field]:
+        if self._msg_spec and self._msg_spec.fields:
+            return self._msg_spec.fields[0]
+
+    def _is_nested(self) -> bool:
+        if self._msg_spec and self._msg_spec.fields:
+            return '/' in str(self._field.type)
+        else:
+            return False
+
+
+def _get_interface_lines(interface_identifier: str) -> typing.Iterable[InterfaceTextLine]:
+
+    parts: typing.List[str] = interface_identifier.split('/')
+    if len(parts) != 3:
+        raise ValueError(
+            f"Invalid name '{interface_identifier}'. Expected three parts separated by '/'"
+        )
+    pkg_name, _, msg_name = parts
+
+    file_path = get_interface_path(interface_identifier)
+    with open(file_path) as file_handler:
+        for line in file_handler:
+            yield InterfaceTextLine(
+                pkg_name=pkg_name,
+                msg_name=msg_name,
+                line_text=line.rstrip(),
+            )
+
+
+def _show_interface(interface_identifier: str, indent_level: int = 0):
+    for line in _get_interface_lines(interface_identifier):
+
+        if str(line):
+            indent_string = indent_level*'\t'
+            print(f'{indent_string}{str(line)}')
+        else:
+            print()
+
+        if line.nested_type:
+            _show_interface(
+                line.nested_type,
+                indent_level=indent_level+1,
+            )
 
 
 class ReadStdinPipe(argparse.Action):
@@ -47,8 +140,6 @@ class ShowVerb(VerbExtension):
 
     def main(self, *, args):
         try:
-            file_path = get_interface_path(args.type)
-        except LookupError as e:
+            _show_interface(args.type)
+        except (ValueError, LookupError) as e:
             return str(e)
-        with open(file_path, 'r', encoding='utf-8') as h:
-            print(h.read().rstrip())
