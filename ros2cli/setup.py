5d5b855369085d02631bc5bdd6ffb7fae680eb5b
Michael Carroll <michael@openrobotics.org>
2020-12-04 15:33:34 -0600

    0.8.8
    Signed-off-by: Michael Carroll <michael@openrobotics.org>

--- ros2cli/setup.py
+++ ros2cli/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ros2cli',
-    version='0.8.7',
+    version='0.8.8',
     packages=find_packages(exclude=['test']),
     extras_require={
         'completion': ['argcomplete'],
