737574d3a45ad615564c302bdf2bd09310fef61c
Tomoya Fujita <Tomoya.Fujita@sony.com>
2021-06-08 03:16:50 +0900

    add '--write' option to avoid an unintentional data loss. (#638)
    * deprecate '--print' and '--output-dir' options.
    
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

--- ros2param/test/test_verb_dump.py
+++ ros2param/test/test_verb_dump.py
@@ -210,24 +210,8 @@ class TestVerbDump(unittest.TestCase):
         )
 
     def test_verb_dump(self):
-        with tempfile.TemporaryDirectory() as tmpdir:
-            with self.launch_param_dump_command(
-                arguments=[f'{TEST_NAMESPACE}/{TEST_NODE}', '--output-dir', tmpdir]
-            ) as param_dump_command:
-                assert param_dump_command.wait_for_shutdown(timeout=TEST_TIMEOUT)
-            assert param_dump_command.exit_code == launch_testing.asserts.EXIT_OK
-            assert launch_testing.tools.expect_output(
-                expected_lines=[''],
-                text=param_dump_command.output,
-                strict=True
-            )
-            # Compare generated parameter file against expected
-            generated_param_file = os.path.join(tmpdir, self._output_file())
-            assert (open(generated_param_file, 'r').read() == EXPECTED_PARAMETER_FILE)
-
-    def test_verb_dump_print(self):
         with self.launch_param_dump_command(
-            arguments=[f'{TEST_NAMESPACE}/{TEST_NODE}', '--print']
+            arguments=[f'{TEST_NAMESPACE}/{TEST_NODE}']
         ) as param_dump_command:
             assert param_dump_command.wait_for_shutdown(timeout=TEST_TIMEOUT)
         assert param_dump_command.exit_code == launch_testing.asserts.EXIT_OK
@@ -236,19 +220,39 @@ class TestVerbDump(unittest.TestCase):
             text=param_dump_command.output,
             strict=True
         )
-        # If both '--output-dir' and '--print' options are provided, ensure it only prints
-        # and no file is written
+
+    # TODO(fujitatomoya): remove this test when '--print' option is removed
+    def test_verb_dump_print(self):
+        # If '--print' is provided, ensure it does nothing but print parameters to stdout
+        with self.launch_param_dump_command(
+            arguments=[f'{TEST_NAMESPACE}/{TEST_NODE}', '--print']
+        ) as param_dump_command:
+            assert param_dump_command.wait_for_shutdown(timeout=TEST_TIMEOUT)
+        assert param_dump_command.exit_code == launch_testing.asserts.EXIT_OK
+        assert launch_testing.tools.expect_output(
+            expected_lines=[
+                "WARNING: '--print' is deprecated; this utility prints to stdout by default"
+            ],
+            text=param_dump_command.output,
+            strict=True
+        )
+
+    # TODO(fujitatomoya): remove this test when '--output-dir' option is removed
+    def test_verb_dump_output(self):
+        # If '--output-dir' is provided, ensure it only saves parameters into file
         with tempfile.TemporaryDirectory() as tmpdir:
             with self.launch_param_dump_command(
-                arguments=[f'{TEST_NAMESPACE}/{TEST_NODE}', '--output-dir', tmpdir, '--print']
+                arguments=[f'{TEST_NAMESPACE}/{TEST_NODE}', '--output-dir', tmpdir]
             ) as param_dump_command:
                 assert param_dump_command.wait_for_shutdown(timeout=TEST_TIMEOUT)
             assert param_dump_command.exit_code == launch_testing.asserts.EXIT_OK
             assert launch_testing.tools.expect_output(
-                expected_text=EXPECTED_PARAMETER_FILE + '\n',
+                expected_lines=[
+                    "WARNING: '--output-dir' is deprecated; use redirection to save to a file"
+                ],
                 text=param_dump_command.output,
                 strict=True
             )
-            # Make sure the file was not create, thus '--print' did preempt
-            not_generated_param_file = os.path.join(tmpdir, self._output_file())
-            assert not os.path.exists(not_generated_param_file)
+            # Compare generated parameter file against expected
+            generated_param_file = os.path.join(tmpdir, self._output_file())
+            assert (open(generated_param_file, 'r').read() == EXPECTED_PARAMETER_FILE)
