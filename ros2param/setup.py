d8e1d9e20d4126882ae46901cbf08b11a4a4c0a4
Jacob Perron <jacob@openrobotics.org>
2022-01-31 14:06:31 -0800

    0.9.11

--- ros2param/setup.py
+++ ros2param/setup.py
@@ -5,7 +5,7 @@ package_name = 'ros2param'
 
 setup(
     name=package_name,
-    version='0.9.10',
+    version='0.9.11',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
