8452aec7bb6d8639aadd17c9eb1eae6b488161e4
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2022-05-20 17:55:52 -0300

    Implement perf tool client
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- perf_tool/src/perf_tool.cpp
+++ perf_tool/src/perf_tool.cpp
@@ -14,6 +14,7 @@
 
 #include <chrono>
 #include <functional>
+#include <memory>
 
 #include "perf_tool_msgs/msg/bytes.hpp"
 
@@ -122,11 +123,81 @@ run_perf_server(rmw_qos_profile_t rmw_sub_qos, std::chrono::nanoseconds experime
   ex.add_node(perf_server);
 
   auto now = start;
+  pybind11::gil_scoped_release gil_guard;
   while (now < start + experiment_duration) {
     ex.spin_once(start + experiment_duration - now);
     now = std::chrono::system_clock::now();
   }
   return perf_server->get_collected_info();
+}
+
+
+class Client : public rclcpp::Node
+{
+public:
+  explicit Client(
+    size_t array_size,
+    std::chrono::nanoseconds target_pub_period,
+    const rclcpp::QoS & pub_qos,
+    const rclcpp::NodeOptions & options = rclcpp::NodeOptions{})
+  : Node("perf_tool_client", options), array_size_{array_size}
+  {
+    pub_ = this->create_publisher<perf_tool_msgs::msg::Bytes>("perf_topic", pub_qos);
+    timer_ = this->create_wall_timer(
+      target_pub_period,
+      std::bind(&Client::pub_next_msg, this));
+    // always keep the vector preallocated, to avoid delays when the timer is triggered
+    bytes_.reserve(array_size);
+  }
+
+  void pub_next_msg()
+  {
+    perf_tool_msgs::msg::Bytes msg;
+    msg.id = next_id;
+    msg.timestamp = std::chrono::system_clock::now().time_since_epoch().count();
+    msg.data = std::move(bytes_);
+    pub_->publish(msg);
+    // always keep the vector preallocated, to avoid delays when the timer is triggered
+    bytes_.resize(array_size_);
+    // published_info_.emplace_back()
+  }
+
+  // std::vector<PerfToolMessageInfo>
+  // get_published_info() {
+  //   return published_info_;
+  // }
+
+private:
+  size_t array_size_;
+  std::vector<unsigned char> bytes_;
+  uint64_t next_id{};
+  std::shared_ptr<rclcpp::TimerBase> timer_;
+  rclcpp::Publisher<perf_tool_msgs::msg::Bytes>::SharedPtr pub_;
+  // std::vector<PerfToolMessageInfo> published_info_;
+};
+
+// TODO(ivanpauno): Split PerfToolMessageInfo in PerfServerMessageInfo and PerfClientMessageInfo.
+// std::vector<PerfToolMessageInfo>
+void
+run_perf_client(
+  size_t array_size,
+  std::chrono::nanoseconds target_pub_period,
+  rmw_qos_profile_t rmw_pub_qos,
+  std::chrono::nanoseconds experiment_duration)
+{
+  auto start = std::chrono::system_clock::now();
+  rclcpp::QoS pub_qos{{rmw_pub_qos.history, rmw_pub_qos.depth}, rmw_pub_qos};
+  auto perf_client = std::make_shared<Client>(array_size, target_pub_period ,pub_qos);
+  rclcpp::executors::SingleThreadedExecutor ex;
+  ex.add_node(perf_client);
+
+  auto now = start;
+  pybind11::gil_scoped_release gil_guard;
+  while (now < start + experiment_duration) {
+    ex.spin_once(start + experiment_duration - now);
+    now = std::chrono::system_clock::now();
+  }
+  // return perf_server->get_collected_info();
 }
 }  // namespace perf_tool
 
@@ -134,6 +205,7 @@ PYBIND11_MODULE(perf_tool_impl, m) {
   m.doc() = "Python wrapper of perf tool implementation";
 
   m.def("run_perf_server", &perf_tool::run_perf_server);
+  m.def("run_perf_client", &perf_tool::run_perf_client);
   // pybind11::class_<perf_tool::Server, std::shared_ptr<perf_tool::Server>>(
     // m, "Server")
   // .def(pybind11::init());
