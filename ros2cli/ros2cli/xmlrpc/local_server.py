053423ec148267020c426077583a4030a1997d50
Tomoya Fujita <Tomoya.Fujita@sony.com>
2022-06-23 16:30:37 -0700

    XMLRPC server accepts request from all local IP addresses.
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

--- ros2cli/ros2cli/xmlrpc/local_server.py
+++ ros2cli/ros2cli/xmlrpc/local_server.py
@@ -18,6 +18,19 @@ import struct
 from xmlrpc.server import SimpleXMLRPCRequestHandler  # noqa
 from xmlrpc.server import SimpleXMLRPCServer
 
+import netifaces
+
+
+def get_local_ipaddrs():
+    iplist = []
+    interfaces = netifaces.interfaces()
+    for interface in interfaces:
+        addrs = netifaces.ifaddresses(interface)
+        if netifaces.AF_INET in addrs.keys():
+            for value in addrs[netifaces.AF_INET]:
+                iplist.append(value['addr'])
+    return iplist
+
 
 class LocalXMLRPCServer(SimpleXMLRPCServer):
 
@@ -37,6 +50,6 @@ class LocalXMLRPCServer(SimpleXMLRPCServer):
         return sock, addr
 
     def verify_request(self, request, client_address):
-        if client_address[0] != '127.0.0.1':
+        if client_address[0] not in get_local_ipaddrs():
             return False
         return super(LocalXMLRPCServer, self).verify_request(request, client_address)
