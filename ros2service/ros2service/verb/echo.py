3b44aa055bbd9386f570e08e7cd48830a427c81e
deepanshu <deepanshubansal01@gmail.com>
2022-07-01 16:35:23 -0400

    deserailize the data before printing
    Signed-off-by: deepanshu <deepanshubansal01@gmail.com>

--- ros2service/ros2service/verb/echo.py
+++ ros2service/ros2service/verb/echo.py
@@ -23,6 +23,7 @@ from rclpy.qos import QoSProfile
 from rclpy.serialization import deserialize_message
 
 from rcl_interfaces.msg import ServiceEventType
+from rcl_interfaces.msg import ServiceEvent
 
 from ros2topic.api import unsigned_int
 
@@ -32,7 +33,7 @@ from ros2service.verb import VerbExtension
 from rosidl_runtime_py.utilities import get_message
 from ros2cli.node.strategy import NodeStrategy
 
-from rosidl_runtime_py import message_to_yaml
+from rosidl_runtime_py import message_to_yaml, message_to_csv
 import yaml
 
 DEFAULT_TRUNCATE_LENGTH = 128
@@ -50,6 +51,8 @@ class EchoVerb(VerbExtension):
         self.no_arr = None
         self.truncate_length = None
         self.flow_style = None
+        self.csv = None
+        self.include_message_info = None
         self.hidden_topic_suffix = "/_service_event"
         self.message_type = get_message("rcl_interfaces/msg/ServiceEvent")
         self.qos_profile = QoSPresetProfiles.get_from_short_key("services_default")
@@ -79,6 +82,16 @@ class EchoVerb(VerbExtension):
         parser.add_argument(
             '--no-str', action='store_true', help="Don't print string fields of messages")
         parser.add_argument(
+            '--csv', action='store_true',
+            help=(
+                'Output all recursive fields separated by commas (e.g. for '
+                'plotting). '
+                'If --include-message-info is also passed, the following fields are prepended: '
+                'source_timestamp, received_timestamp, publication_sequence_number,'
+                ' reception_sequence_number.'
+            )
+        )
+        parser.add_argument(
             '--include-message-info', '-i', action='store_true',
             help='Shows the associated message info.')
 
@@ -87,13 +100,14 @@ class EchoVerb(VerbExtension):
         self.truncate_length = args.truncate_length if not args.full_length else None
         self.no_arr = args.no_arr
         self.no_str = args.no_str
+        self.csv = args.csv
+        self.include_message_info = args.include_message_info
 
         try:
             parts = args.service_type.split('/')
             if len(parts) == 2:
                 parts = [parts[0], 'srv', parts[1]]
             package_name = parts[0]
-            print(parts[:-1])
             module = importlib.import_module('.'.join(parts[:-1]))
             srv_name = parts[-1]
             self.srv_module = getattr(module, srv_name)
@@ -130,13 +144,36 @@ class EchoVerb(VerbExtension):
 
     def _subscriber_callback(self, msg, info):
         service_event_type = msg.info.event_type
-        serialized_event = b''.join(msg.serialized_event)
+        serialize_msg_type = None
 
         if service_event_type is ServiceEventType.REQUEST_RECEIVED or \
                 service_event_type is ServiceEventType.REQUEST_SENT:
-            service_request = deserialize_message(serialized_event,
-                                                  self.srv_module.Request)
+            serialize_msg_type = self.srv_module.Request
         elif service_event_type is ServiceEventType.RESPONSE_RECEIVED or \
                 service_event_type is ServiceEventType.RESPONSE_SENT:
-            service_request = deserialize_message(serialized_event,
-                                                  self.srv_module.Response)
+            serialize_msg_type = self.srv_module.Response
+        else:  # TODO remove this else condition later
+            print("# Invalid Type")
+            return
+
+        # csv: TODO
+        if self.csv:
+            to_print = message_to_csv(
+                msg,
+                truncate_length=self.truncate_length,
+                no_arr=self.no_arr,
+                no_str=self.no_str)
+            if self.include_message_info:
+                to_print = f'{",".join(str(x) for x in info.values())},{to_print}'
+            print(to_print)
+            return
+
+        # yaml
+        if self.include_message_info:
+            print(yaml.dump(info), end='---\n')
+        print(
+            message_to_yaml(
+                msg, truncate_length=self.truncate_length,
+                no_arr=self.no_arr, no_str=self.no_str, flow_style=self.flow_style,
+                serialize_msg_type=serialize_msg_type),
+            end='---\n')
