0ff9423ebbfb606c7a4675c8c614775984438e89
Shane Loretz <sloretz@osrfoundation.org>
2022-10-05 13:43:38 -0700

    Update test_api for group and port args
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- ros2multicast/test/test_api.py
+++ ros2multicast/test/test_api.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import random
 import threading
 import time
 
@@ -19,17 +20,79 @@ from ros2multicast.api import receive
 from ros2multicast.api import send
 
 
-def test_api():
-    sent_data = b'test_api'
+def _send_receive(sent_data, rx_kwargs, tx_kwargs):
     received_data = None
 
     def target():
         nonlocal received_data
-        received_data, _ = receive(timeout=1.0)
+        try:
+            received_data, _ = receive(**rx_kwargs)
+        except TimeoutError:
+            pass
 
     t = threading.Thread(target=target)
     t.start()
     time.sleep(0.1)
-    send(sent_data)
+    send(sent_data, **tx_kwargs)
     t.join()
-    assert sent_data == received_data
+    return received_data
+
+
+def test_api():
+    sent_data = b'test_api'
+
+    rx_kwargs = {'timeout': 1.0}
+    tx_kwargs = {}
+
+    assert sent_data == _send_receive(sent_data, rx_kwargs, tx_kwargs)
+
+
+def test_group_and_port():
+    sent_data = b'test_api'
+
+    r1 = random.randrange(0, 255)
+    r2 = random.randrange(0, 255)
+    r3 = random.randrange(1, 255)
+    port = random.randrange(16000, 18000)
+
+    group = f'234.{r1}.{r2}.{r3}'
+
+    tx_kwargs = {'group': group, 'port': port}
+    rx_kwargs = {'timeout': 1.0}
+    rx_kwargs.update(tx_kwargs)
+
+    assert sent_data == _send_receive(sent_data, rx_kwargs, tx_kwargs)
+
+
+def test_group_mismatch():
+    sent_data = b'test_api'
+
+    r1 = random.randrange(0, 255)
+    r2 = random.randrange(0, 255)
+    r3 = random.randrange(1, 255)
+    port = random.randrange(16000, 18000)
+
+    group1 = f'234.{r1}.{r2}.{r3}'
+    group2 = f'235.{r1}.{r2}.{r3}'
+
+    tx_kwargs = {'group': group1, 'port': port}
+    rx_kwargs = {'group': group2, 'port': port, 'timeout': 1.0}
+
+    assert _send_receive(sent_data, rx_kwargs, tx_kwargs) is None
+
+
+def test_port_mismatch():
+    sent_data = b'test_api'
+
+    r1 = random.randrange(0, 255)
+    r2 = random.randrange(0, 255)
+    r3 = random.randrange(1, 255)
+    port1 = random.randrange(16000, 18000)
+    port2 = port1 + 1
+
+    group = f'234.{r1}.{r2}.{r3}'
+
+    tx_kwargs = {'group': group, 'port': port1}
+    rx_kwargs = {'group': group, 'port': port2, 'timeout': 1.0}
+
+    assert _send_receive(sent_data, rx_kwargs, tx_kwargs) is None
