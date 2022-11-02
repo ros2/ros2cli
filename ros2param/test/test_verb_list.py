be002dfba489fb98038f168527c6407b10f108ed
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2022-11-02 10:26:18 -0500

    Mark TestCase subclass as xfail
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- ros2param/test/test_verb_list.py
+++ ros2param/test/test_verb_list.py
@@ -84,7 +84,8 @@ def generate_test_description(rmw_implementation):
         ),
     ])
 
-
+# Flaky on Galactic: https://github.com/ros2/ros2cli/issues/630
+@pytest.mark.xfail
 class TestVerbList(unittest.TestCase):
 
     @classmethod
