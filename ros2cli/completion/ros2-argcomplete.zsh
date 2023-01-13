aadc580fe16925ba18895067468373aa6669e0de
mjbogusz <mjbogusz@users.noreply.github.com>
2023-01-13 01:30:22 +0100

    ZSH argcomplete: call compinit only if needed (#750)
    Signed-off-by: Maciej Bogusz <mjbogusz+github@gmail.com>
    
    Signed-off-by: Maciej Bogusz <mjbogusz+github@gmail.com>

--- ros2cli/completion/ros2-argcomplete.zsh
+++ ros2cli/completion/ros2-argcomplete.zsh
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-autoload -U +X compinit && compinit
+if (( ! ${+_comps} )); then
+  autoload -U +X compinit && compinit
+fi
 autoload -U +X bashcompinit && bashcompinit
 
 # Get this scripts directory
