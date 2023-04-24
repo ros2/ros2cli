b597f4d100bb504a7478b6c963ee609f5cff9d0a
David V. Lu <davidvlu@gmail.com>
2023-04-24 10:09:04 -0400

    Flake, added tests, increased timeout
    Signed-off-by: David V. Lu <davidvlu@gmail.com>

--- ros2interface/ros2interface/verb/package.py
+++ ros2interface/ros2interface/verb/package.py
@@ -11,7 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import collections
+
 from ros2interface.api import package_name_completer
 from ros2interface.verb import VerbExtension
 from rosidl_runtime_py import get_action_interfaces, get_interfaces
