95496c12119bd7639038d082cfb8ba927db9a0a5
Anthony Welte <tony.welte@gmail.com>
2024-11-05 22:04:54 +0100

    Fix style according to flake8
    Signed-off-by: Anthony Welte <tony.welte@gmail.com>

--- ros2topic/ros2topic/api/__init__.py
+++ ros2topic/ros2topic/api/__init__.py
@@ -263,10 +263,11 @@ def add_qos_arguments(
             'with (overrides liveliness lease duration value of --qos-profile option, default: '
             f'{default_profile.liveliness_lease_duration})'))
 
+
 def extract_qos_arguments(args):
     class QosArgs:
         pass
-    
+
     qos = QosArgs()
     qos.qos_profile = args.qos_profile
     qos.qos_reliability = args.qos_reliability
@@ -277,6 +278,7 @@ def extract_qos_arguments(args):
     qos.qos_liveliness_lease_duration_seconds = args.qos_liveliness_lease_duration_seconds
 
     return qos
+
 
 def choose_qos(node, topic_name: str, qos_args):
     if (qos_args.qos_reliability is not None or
