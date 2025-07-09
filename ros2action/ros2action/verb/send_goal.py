68806d3fe333284b7e0092e9ede388cef936063e
Tomoya Fujita <Tomoya.Fujita@sony.com>
2025-07-09 10:55:59 -0700

    ros2 action send_goal -t / --timeout option support.
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

--- ros2action/ros2action/verb/send_goal.py
+++ ros2action/ros2action/verb/send_goal.py
@@ -53,6 +53,12 @@ class SendGoalVerb(VerbExtension):
         parser.add_argument(
             '-f', '--feedback', action='store_true',
             help='Echo feedback messages for the goal')
+        parser.add_argument(
+            '-t', '--timeout', metavar='N', type=int, default=None,
+            help=(
+                'Wait for N seconds until server becomes available and goal is completed '
+                '(default waits indefinitely)'
+            ))
 
     def main(self, *, args):
         feedback_callback = None
@@ -64,7 +70,7 @@ class SendGoalVerb(VerbExtension):
         else:
             goal = args.goal
 
-        return send_goal(args.action_name, args.action_type, goal, feedback_callback)
+        return send_goal(args.action_name, args.action_type, goal, feedback_callback, args.timeout)
 
 
 def _goal_status_to_string(status):
@@ -88,7 +94,7 @@ def _feedback_callback(feedback):
     print('Feedback:\n    {}'.format(message_to_yaml(feedback.feedback)))
 
 
-def send_goal(action_name, action_type, goal_values, feedback_callback):
+def send_goal(action_name, action_type, goal_values, feedback_callback, timeout=None):
     goal_handle = None
     node = None
     action_client = None
@@ -117,7 +123,9 @@ def send_goal(action_name, action_type, goal_values, feedback_callback):
             return 'Failed to populate message fields: {!r}'.format(ex)
 
         print('Waiting for an action server to become available...')
-        action_client.wait_for_server()
+        if not action_client.wait_for_server(timeout_sec=timeout):
+            print('Action server is not available after waiting {timeout} seconds.')
+            return
 
         stamp_now = node.get_clock().now().to_msg()
         for field_setter in timestamp_fields:
@@ -168,7 +176,11 @@ def send_goal(action_name, action_type, goal_values, feedback_callback):
         print('Goal accepted with ID: {}\n'.format(bytes(goal_handle.goal_id.uuid).hex()))
 
         result_future = goal_handle.get_result_async()
-        rclpy.spin_until_future_complete(node, result_future)
+        rclpy.spin_until_future_complete(node, result_future, timeout_sec=timeout)
+
+        if not result_future.done():
+            print('Timed out waiting for result after {timeout} seconds.')
+            return
 
         result = result_future.result()
 
