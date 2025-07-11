ce454a800c3561ad2fbb975a5aed6eedab654590
mosfet80 <10235105+mosfet80@users.noreply.github.com>
2025-07-11 15:03:22 +0200

    fix setuptools deprecations (#1066)
    Signed-off-by: mosFet <realeandrea@yahoo.it>
    (cherry picked from commit 8a90dbeeebc7a6022bd6794a540284ac768ad8f4)

--- ros2service/setup.py
+++ ros2service/setup.py
@@ -24,14 +24,17 @@ setup(
     classifiers=[
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
     ],
     description='The service command for ROS 2 command line tools.',
     long_description="""\
 The package provides the service command for the ROS 2 command line tools.""",
     license='Apache License, Version 2.0',
-    tests_require=['pytest'],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'ros2cli.command': [
             'service = ros2service.command.service:ServiceCommand',
