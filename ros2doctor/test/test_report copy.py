9187f6d0dc893f6dd42b23b7bcce4f0c1a96c057
Michael Carlstrom <rmc@carlstrom.com>
2025-08-05 21:27:54 -0700

    test imp
    Signed-off-by: Michael Carlstrom <rmc@carlstrom.com>

---
+++ ros2doctor/test/test_report copy.py
@@ -0,0 +1,160 @@
+# Copyright 2025 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import contextlib
+import os
+import sys
+from typing import Any
+from typing import Dict
+from typing import Generator
+from typing import Tuple
+import unittest
+
+from launch import LaunchDescription
+from launch import LaunchService
+from launch.actions import ExecuteProcess
+
+from launch.actions import SetEnvironmentVariable
+from launch.actions import UnsetEnvironmentVariable
+
+import launch_testing
+import launch_testing.actions
+import launch_testing.asserts
+import launch_testing.markers
+import launch_testing.tools
+import launch_testing_ros.tools
+
+import pytest
+
+from rclpy.utilities import get_available_rmw_implementations
+from ros2cli.helpers import get_rmw_additional_env
+
+
+# Skip cli tests on Windows while they exhibit pathological behavior
+# https://github.com/ros2/build_farmer/issues/248
+if sys.platform.startswith('win'):
+    pytest.skip(
+        'CLI tests can block for a pathological amount of time on Windows.',
+        allow_module_level=True)
+
+
+CYCLONEDDS_XML = '<CycloneDDS><Domain><General></General></Domain></CycloneDDS>'
+
+
+@pytest.mark.rostest
+@launch_testing.parametrize('rmw_implementation', get_available_rmw_implementations())
+@launch_testing.markers.keep_alive
+def generate_test_description(rmw_implementation: str) -> Tuple[LaunchDescription,
+                                                                Dict[str, Any]]:
+    path_to_fixtures = os.path.join(os.path.dirname(__file__), 'fixtures')
+    additional_env = get_rmw_additional_env(rmw_implementation)
+    additional_env['PYTHONUNBUFFERED'] = '1'
+
+    return LaunchDescription([
+        ExecuteProcess(
+            cmd=['ros2', 'daemon', 'stop'],
+            name='daemon-stop',
+            on_exit=[
+                ExecuteProcess(
+                    cmd=['ros2', 'daemon', 'start'],
+                    name='daemon-start',
+                    additional_env=additional_env,
+                    on_exit=[
+                        UnsetEnvironmentVariable('ROS_AUTOMATIC_DISCOVERY_RANGE'),
+                        SetEnvironmentVariable('ROS_DISTRO', 'rolling'),
+                        SetEnvironmentVariable('ROS_HOME', 'BAR'),
+                        SetEnvironmentVariable('ROS_LOG_DIR', 'BAZ'),
+                        SetEnvironmentVariable('RCUTILS_COLORIZED_OUTPUT', '0'),
+                        SetEnvironmentVariable('FASTDDS_BUILTIN_TRANSPORTS', 'UDPv6'),
+                        SetEnvironmentVariable('RUST_LOG', 'ZENOHBOO'),
+                        SetEnvironmentVariable('RMW_CONNEXT_INITIAL_PEERS', 'CONNEXTBOO'),
+                        SetEnvironmentVariable('CYCLONEDDS_URI', CYCLONEDDS_XML),
+                        launch_testing.actions.ReadyToTest()
+                    ]
+                )
+            ]
+        )
+    ]), locals()
+
+
+class TestEnvironmentReport(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(
+            cls,
+            launch_service: LaunchService,
+            proc_info: launch_testing.tools.process.ActiveProcInfoHandler,
+            proc_output: launch_testing.tools.process.ActiveIoHandler,
+            rmw_implementation: str,
+    ) -> None:
+        cls.rmw_implementation = rmw_implementation
+        rmw_implementation_filter = launch_testing_ros.tools.basic_output_filter(
+            filtered_patterns=['WARNING: topic .* does not appear to be published yet'],
+            filtered_rmw_implementation=rmw_implementation
+        )
+
+        @contextlib.contextmanager
+        def launch_doctor_command(
+            self,
+            arguments
+        ) -> Generator[launch_testing.tools.process.ProcessProxy, None, None]:
+            additional_env = get_rmw_additional_env(rmw_implementation)
+            additional_env['PYTHONUNBUFFERED'] = '1'
+            doctor_command_action = ExecuteProcess(
+                cmd=['ros2', 'doctor', *arguments],
+                additional_env=additional_env,
+                name='ros2doctor-cli',
+                output='screen'
+            )
+            with launch_testing.tools.launch_process(
+                    launch_service, doctor_command_action, proc_info, proc_output,
+                    output_filter=rmw_implementation_filter
+            ) as doctor_command:
+                yield doctor_command
+
+        cls.launch_doctor_command = launch_doctor_command
+
+        if rmw_implementation == 'rmw_cyclonedds_cpp':
+            cls.expected_line = f'CYCLONEDDS_URI={CYCLONEDDS_XML}'
+        elif rmw_implementation == 'rmw_connext_cpp':
+            cls.expected_line = 'RMW_CONNEXT_INITIAL_PEERS=CONNEXTBOO'
+        elif rmw_implementation == 'rmw_zenoh_cpp':
+            cls.expected_line = 'RUST_LOG=ZENOHBOO'
+        elif rmw_implementation == 'rmw_fastrtps_cpp':
+            cls.expected_line = 'FASTDDS_BUILTIN_TRANSPORTS=UDPv6'
+        else:
+            raise ValueError(f'Unsupported rmw={rmw_implementation}')
+
+    @launch_testing.markers.retry_on_failure(times=5, delay=1)
+    def test_report(self) -> None:
+        # TODO(@fujitatomoya): rmw_zenoh_cpp is instable to find the endpoints, it does not
+        # matter if DaemonNode or DirectNode is used. For now, skip the test for rmw_zenoh_cpp.
+        if self.rmw_implementation == 'rmw_zenoh_cpp':
+            raise unittest.SkipTest()
+
+        for argument in ['-r', '--report']:
+            with self.launch_doctor_command(
+                    arguments=[argument]
+            ) as doctor_command:
+                assert doctor_command.wait_for_shutdown(timeout=10)
+            assert doctor_command.exit_code == launch_testing.asserts.EXIT_OK
+
+            assert launch_testing.tools.expect_output(
+                expected_lines=[
+                    'ros environment variables                 : ROS_DISTRO=rolling, ROS_HOME=BAR, ROS_LOG_DIR=BAZ',
+                    'rcutils environment variables    : RCUTILS_COLORIZED_OUTPUT=0',
+                    f'{self.rmw_implementation} environment variables : {self.expected_line}'
+                ],
+                text=doctor_command.output
+            )
