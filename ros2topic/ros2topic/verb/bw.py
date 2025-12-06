48f597d5564129366c8051ce3b3740d654bd3579
Tomoya Fujita <Tomoya.Fujita@sony.com>
2025-12-06 14:19:18 +0900

    support multiple topics for "ros2 topic bw". (#1124)
    * support multiple topics for "ros2 topic bw".
    
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>
    
    * address copilot review comments.
    
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>
    
    * Fix flake8
    
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>
    
    * take care of round-up case for test_use_sim_time.
    
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>
    
    * always print 'Subscribed to [topic name]' even with single topic.
    
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>
    
    * make linters happy
    
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>
    
    ---------
    
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>
    Co-authored-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- ros2topic/ros2topic/verb/bw.py
+++ ros2topic/ros2topic/verb/bw.py
@@ -29,11 +29,17 @@
 # This file is originally from:
 # https://github.com/ros/ros_comm/blob/6e5016f4b2266d8a60c9a1e163c4928b8fc7115e/tools/rostopic/src/rostopic/__init__.py
 
+from collections import defaultdict
+
+import functools
 import sys
 import threading
+import time
 import traceback
 
 import rclpy
+
+from rclpy.executors import ExternalShutdownException
 
 from ros2cli.node.direct import add_arguments as add_direct_node_arguments
 from ros2cli.node.direct import DirectNode
@@ -73,7 +79,8 @@ class BwVerb(VerbExtension):
         )
         arg = parser.add_argument(
             'topic_name',
-            help='Topic name to monitor for bandwidth utilization')
+            nargs='+',
+            help="Names of the ROS topics to monitor for bandwidth utilization (e.g. '/chatter')")
         arg.completer = TopicNameCompleter(
             include_hidden_topics_key='include_hidden_topics')
         add_qos_arguments(parser, 'subscribe', 'sensor_data')
@@ -84,106 +91,209 @@ class BwVerb(VerbExtension):
         add_direct_node_arguments(parser)
 
     def main(self, *, args):
-        with DirectNode(args) as node:
-            qos_profile = choose_qos(node.node, topic_name=args.topic_name, qos_args=args)
-            _rostopic_bw(node.node, args.topic_name, qos_profile, window_size=args.window_size)
+        return main(args)
+
+
+def main(args):
+    topics = args.topic_name
+
+    with DirectNode(args) as node:
+        _rostopic_bw(node.node, topics, qos_args=args, window_size=args.window_size)
 
 
 class ROSTopicBandwidth(object):
 
     def __init__(self, node, window_size):
         self.lock = threading.Lock()
-        self.last_printed_tn = 0
-        self.sizes = []
-        self.times = []
+        self._last_printed_tn = defaultdict(int)
+        self._sizes = defaultdict(list)
+        self._times = defaultdict(list)
         self.window_size = window_size
-        self.use_sim_time = node.get_parameter('use_sim_time').value
-        self.clock = node.get_clock()
+        self._clock = node.get_clock()
 
-    def callback(self, data):
+    def get_last_printed_tn(self, topic=None):
+        return self._last_printed_tn[topic]
+
+    def set_last_printed_tn(self, value, topic=None):
+        self._last_printed_tn[topic] = value
+
+    def get_sizes(self, topic=None):
+        return self._sizes[topic]
+
+    def set_sizes(self, value, topic=None):
+        self._sizes[topic] = value
+
+    def get_times(self, topic=None):
+        return self._times[topic]
+
+    def set_times(self, value, topic=None):
+        self._times[topic] = value
+
+    def callback(self, data, topic=None):
         """Execute ros sub callback."""
         with self.lock:
             try:
-                t = self.clock.now()
-                self.times.append(t)
+                t = self._clock.now()
+                self.get_times(topic=topic).append(t)
                 # TODO(yechun1): Subscribing to the msgs and calculate the length may be
                 # inefficient. Optimize here if a better solution is found.
-                self.sizes.append(len(data))  # AnyMsg instance
-                assert len(self.times) == len(self.sizes)
+                self.get_sizes(topic=topic).append(len(data))  # AnyMsg instance
+                assert len(self.get_times(topic=topic)) == len(self.get_sizes(topic=topic))
 
-                if len(self.times) > self.window_size:
-                    self.times.pop(0)
-                    self.sizes.pop(0)
+                if len(self.get_times(topic=topic)) > self.window_size:
+                    self.get_times(topic=topic).pop(0)
+                    self.get_sizes(topic=topic).pop(0)
             except Exception:
                 traceback.print_exc()
 
-    def get_bw(self):
+    def get_bw(self, topic=None):
         """Get the average publishing bw."""
-        if len(self.times) < 2:
-            return None, None, None, None, None
         with self.lock:
-            n = len(self.times)
-            tn = self.clock.now()
-            t0 = self.times[0]
+            times = self.get_times(topic=topic)
+            sizes = self.get_sizes(topic=topic)
+            if len(times) < 2:
+                return None, None, None, None, None
+            if topic is not None:
+                last_time = times[-1]
+                if last_time == self.get_last_printed_tn(topic=topic):
+                    return None, None, None, None, None
+            n = len(times)
+            tn = self._clock.now()
+            t0 = times[0]
             if tn <= t0:
                 print('WARNING: time is reset!', file=sys.stderr)
-                self.times = []
-                self.sizes = []
+                self.set_times([], topic=topic)
+                self.set_sizes([], topic=topic)
                 return None, None, None, None, None
 
-            total = sum(self.sizes)
+            total = sum(sizes)
             bytes_per_s = total / ((tn.nanoseconds - t0.nanoseconds) * 1.e-9)
             mean = total / n
 
             # min and max
-            max_s = max(self.sizes)
-            min_s = min(self.sizes)
+            max_s = max(sizes)
+            min_s = min(sizes)
+
+            if topic is not None:
+                self.set_last_printed_tn(times[-1], topic=topic)
 
         return bytes_per_s, n, mean, min_s, max_s
 
-    def print_bw(self):
+    def print_bw(self, topics=None):
         """Print the average publishing bw to screen."""
-        (bytes_per_s, n, mean, min_s, max_s) = self.get_bw()
-        if bytes_per_s is None:
+        def get_format_bw(stat):
+            bytes_per_s, n, mean, min_s, max_s = stat
+            # min/max and even mean are likely to be much smaller,
+            # but for now I prefer unit consistency
+            if bytes_per_s < 1000:
+                bw, mean_str, min_str, max_str = map(
+                    str_bytes, (bytes_per_s, mean, min_s, max_s))
+            elif bytes_per_s < 1000000:
+                bw, mean_str, min_str, max_str = map(
+                    str_kilobytes, (bytes_per_s, mean, min_s, max_s))
+            else:
+                bw, mean_str, min_str, max_str = map(
+                    str_megabytes, (bytes_per_s, mean, min_s, max_s))
+            # Bandwidth is per second
+            bw += '/s'
+            return bw, mean_str, min_str, max_str, n
+
+        if topics is None or len(topics) == 1:
+            topic = topics[0] if topics else None
+            ret = self.get_bw(topic)
+            if ret[0] is None:
+                return
+            bw, mean_str, min_str, max_str, n = get_format_bw(ret)
+            print(f'{bw} from {n} messages\n\tMessage size mean: {mean_str} ' +
+                  f'min: {min_str} max: {max_str}')
             return
 
-        # min/max and even mean are likely to be much smaller,
-        # but for now I prefer unit consistency
-        if bytes_per_s < 1000:
-            bw, mean, min_s, max_s = map(str_bytes, (bytes_per_s, mean, min_s, max_s))
-        elif bytes_per_s < 1000000:
-            bw, mean, min_s, max_s = map(str_kilobytes, (bytes_per_s, mean, min_s, max_s))
-        else:
-            bw, mean, min_s, max_s = map(str_megabytes, (bytes_per_s, mean, min_s, max_s))
+        # monitoring multiple topics' bw
+        header = ['topic', 'bandwidth', 'window', 'mean', 'min', 'max']
+        stats = {h: [] for h in header}
+        for topic in topics:
+            bw_stat = self.get_bw(topic)
+            if bw_stat[0] is None:
+                continue
+            bw, mean_str, min_str, max_str, n = get_format_bw(bw_stat)
+            stats['topic'].append(topic)
+            stats['bandwidth'].append(bw)
+            stats['window'].append(str(n))
+            stats['mean'].append(mean_str)
+            stats['min'].append(min_str)
+            stats['max'].append(max_str)
+        if not stats['topic']:
+            return
+        print(_get_ascii_table(header, stats))
 
-        # Bandwidth is per second
-        bw += '/s'
 
-        print(f'{bw} from {n} messages\n\tMessage size mean: {mean} min: {min_s} max: {max_s}')
+def _get_ascii_table(header, cols):
+    # compose table with left alignment
+    if not cols or not cols[header[0]]:
+        return ''
+    header_aligned = []
+    col_widths = []
+    for h in header:
+        col_width = max(len(h), max(len(el) for el in cols[h]))
+        col_widths.append(col_width)
+        header_aligned.append(h.center(col_width))
+        for i, el in enumerate(cols[h]):
+            cols[h][i] = str(cols[h][i]).ljust(col_width)
+    # sum of col and each 3 spaces width
+    table_width = sum(col_widths) + 3 * (len(header) - 1)
+    n_rows = len(cols[header[0]])
+    body = '\n'.join('   '.join(cols[h][i] for h in header) for i in range(n_rows))
+    table = '{header}\n{hline}\n{body}\n'.format(
+        header='   '.join(header_aligned), hline='=' * table_width, body=body)
+    return table
 
 
-def _rostopic_bw(node, topic, qos_profile, window_size=DEFAULT_WINDOW_SIZE):
-    """Periodically print the received bandwidth of a topic to console until shutdown."""
+def _rostopic_bw(node, topics, qos_args, window_size=DEFAULT_WINDOW_SIZE):
+    """Periodically print the received bandwidth of topics to console until shutdown."""
     # pause bw until topic is published
-    msg_class = get_msg_class(node, topic, blocking=True, include_hidden_topics=True)
-    if msg_class is None:
+    rt = ROSTopicBandwidth(node, window_size)
+    topics_to_be_removed = []
+    for topic in topics:
+        msg_class = get_msg_class(
+            node, topic, blocking=True, include_hidden_topics=True)
+
+        if msg_class is None:
+            topics_to_be_removed.append(topic)
+            print('WARNING: failed to find message type for topic [%s]' % topic)
+            continue
+
+        qos_profile = choose_qos(node, topic_name=topic, qos_args=qos_args)
+
+        node.create_subscription(
+            msg_class,
+            topic,
+            functools.partial(rt.callback, topic=topic),
+            qos_profile,
+            raw=True
+        )
+        print('Subscribed to [%s]' % topic)
+
+    # remove the topics from the list if failed to find message type
+    for topic in topics_to_be_removed:
+        topics.remove(topic)
+    if len(topics) == 0:
         node.destroy_node()
+        rclpy.try_shutdown()
         return
 
-    rt = ROSTopicBandwidth(node, window_size)
-    node.create_subscription(
-        msg_class,
-        topic,
-        rt.callback,
-        qos_profile,
-        raw=True
-    )
+    try:
+        def thread_func():
+            while rclpy.ok():
+                rt.print_bw(topics)
+                time.sleep(1.0)
 
-    print(f'Subscribed to [{topic}]')
-    timer = node.create_timer(1, rt.print_bw)
-    while rclpy.ok():
-        rclpy.spin_once(node)
-
-    node.destroy_timer(timer)
-    node.destroy_node()
-    rclpy.shutdown()
+        print_thread = threading.Thread(target=thread_func)
+        print_thread.start()
+        rclpy.spin(node)
+    except (KeyboardInterrupt, ExternalShutdownException):
+        # Suppress shutdown exceptions; cleanup is handled in finally block.
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.try_shutdown()
+        print_thread.join()
