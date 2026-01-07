717f3cf138e08513983f032b5d62cbb82953cc9f
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-01-07 17:08:34 +0100

    0.32.7

--- ros2cli/setup.py
+++ ros2cli/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ros2cli',
-    version='0.32.6',
+    version='0.32.7',
     packages=find_packages(exclude=['test']),
     extras_require={
         'completion': ['argcomplete'],
