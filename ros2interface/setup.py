030edbc9156f6ad99058914ba2f6847aeb35183a
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-03-06 22:55:06 +0100

    0.18.18

--- ros2interface/setup.py
+++ ros2interface/setup.py
@@ -5,7 +5,7 @@ package_name = 'ros2interface'
 
 setup(
     name=package_name,
-    version='0.18.17',
+    version='0.18.18',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
