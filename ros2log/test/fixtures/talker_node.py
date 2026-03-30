63a208dbdf0a13f9663470600ebb9f17fd012c4e
Decwest <fumiyaonishi1016@gmail.com>
2026-03-30 22:41:24 +0900

    fix copyright year
    Signed-off-by: Decwest <fumiyaonishi1016@gmail.com>

--- ros2log/test/fixtures/talker_node.py
+++ ros2log/test/fixtures/talker_node.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2025 Tomoya Fujita, Fumiya Ohnishi
+# Copyright 2026 Tomoya Fujita, Fumiya Ohnishi
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
